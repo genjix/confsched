49a3f3489a27caf0b636ce16fa03135cace03658
genjix <genjix@nite.(none)>
2012-06-04 21:02:11 +0200

    conference scheduler for bitcoin2012.com

---
+++ confsched.py
@@ -0,0 +1,95 @@
+import os, sys, pygame, random
+
+def write_schedule(filename, schedule):
+    with open(filename, "w") as schedule_file:
+        for name in schedule:
+            schedule_file.write("%s\n" % name)
+
+class SpeakerIterator:
+
+    def __init__(self, all_speakers):
+        self.all_speakers = all_speakers
+        self.reset()
+
+    def next(self):
+        try:
+            self.current_speaker = self.it.next()
+        except StopIteration:
+            self.reset()
+            self.next()
+
+    def reset(self):
+        self.it = iter(self.all_speakers.items())
+
+    @property
+    def current_name(self):
+        return self.current_speaker[0]
+    @property
+    def current_tile(self):
+        return self.current_speaker[1][0]
+    @property
+    def current_label(self):
+        return self.current_speaker[1][1]
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(-1)
+
+    number_slots = 80
+    slots_per_row = number_slots / 4
+    tile_size = 32
+
+    pygame.init()
+    screen = pygame.display.set_mode((slots_per_row * tile_size, 600))
+    font = pygame.font.SysFont("Comic Sans MS", 20)
+
+    speakers = {}
+    for filename in os.listdir("speakers"):
+        speakers[filename] = \
+            (pygame.image.load("speakers/" + filename),
+             font.render(filename, 1, (180, 180, 180)))
+
+    label_selected = font.render("selected speaker:", 1, (100, 100, 100))
+
+    selected_speaker = SpeakerIterator(speakers)
+    selected_speaker.next()
+
+    schedule = ["none.png"]* number_slots
+    schedule_filename = sys.argv[1]
+    with open(schedule_filename) as schedule_file:
+        for i, name in enumerate(schedule_file):
+            schedule[i] = name.strip("\n")
+            print "#", i, name.strip("\n")
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                write_schedule(schedule_filename, schedule)
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    selected_speaker.next()
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos[0] // tile_size, event.pos[1] // tile_size
+                i = y * slots_per_row + x
+                print i, "set to", selected_speaker.current_name
+                schedule[i] = selected_speaker.current_name
+
+        screen.fill((0, 0, 0))
+        assert len(schedule) == number_slots
+        for i in range(number_slots):
+            x = (i % slots_per_row) * tile_size
+            y = (i // slots_per_row) * tile_size
+            tile_name = schedule[i]
+            tile = speakers[tile_name][0]
+            screen.blit(tile, (x, y))
+        for i, speaker in enumerate(speakers.items()):
+            y = i * tile_size + 200
+            screen.blit(speaker[1][0], (10, y))
+            screen.blit(speaker[1][1], (50, y))
+        screen.blit(label_selected, (10, 520))
+        screen.blit(selected_speaker.current_tile, (10, 550))
+        screen.blit(selected_speaker.current_label, (50, 550))
+        pygame.display.flip()
+        pygame.time.wait(100)
+
